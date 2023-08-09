5c0b2510ff76272a3c1f097737f3d48d30af813a
Andrei Slapik <Slapon4ik92@gmail.com>
2023-08-09 18:46:09 +0300

    add solution 4

---
+++ 4.js
@@ -0,0 +1,11 @@
+/* You should not change code above the specified comment.
+b should have a value of 7.
+a should be assigned to b with =. */
+
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+b = a;
