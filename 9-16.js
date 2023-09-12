97016fbef89b34b4d25ed7637ce2cc2db7fc9f2b
Andrei Slapik <Slapon4ik92@gmail.com>
2023-09-12 12:04:08 +0300

    add solution 9-16

---
+++ 9-16.js
@@ -0,0 +1,83 @@
+//Update the code so it only uses the let keyword.
+
+var catName = "Oliver";
+var catSound = "Meow!";
+
+//Solution
+
+let catName = "Oliver";
+let catSound = "Meow!";
+//##################################################
+
+//Change the code so that all variables are declared using let or const. Use let when you want the variable to change, and const when you want the variable to remain constant. Also, rename variables declared with const to conform to common practices. Do not change the strings assigned to the variables.
+
+var fCC = "freeCodeCamp"; // Change this line
+var fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(fCC, fact); // Change this line
+
+//Solution
+
+const FCC = "freeCodeCamp";
+let fact = "is cool!"; 
+fact = "is awesome!";
+console.log(FCC, fact);
+//##################################################
+
+//Add Two Numbers with JavaScript
+//Change the 0 so that sum will equal 20.
+
+//const sum = 10 + 0;
+
+//Solution
+
+const sum = 10 + 10;
+//##################################################
+
+//Subtract One Number from Another with JavaScript
+//Change the 0 so the difference is 12.
+
+//const difference = 45 - 0;
+
+//Solution
+
+const difference = 45 - 33;
+//##################################################
+
+//Multiply Two Numbers with JavaScript
+//Change the 0 so that product will equal 80.
+
+//const product = 8 * 0;
+
+//Solution
+
+const product = 8 * 10;
+//##################################################
+
+//Divide One Number by Another with JavaScript.
+//Change the 0 so that the quotient is equal to 2.
+
+//const quotient = 66 / 0;
+
+//Solution
+
+const quotient = 66 / 33;
+//##################################################
+
+//Increment a Number with JavaScript. 
+//Change the code to use the ++ operator on myVar.
+
+let myVar = 87;
+
+//Solution
+
+myVar++;
+//##################################################
+
+//Decrement a Number with JavaScript
+//Change the code to use the -- operator on myVar.
+
+let myVar2 = 11;
+
+//Solution
+myVar2--;
