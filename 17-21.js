7bcc855f745ad648ce922e57d65fbda72853dbeb
Andrei Slapik <Slapon4ik92@gmail.com>
2023-09-13 11:32:25 +0300

    add solution 17-21

---
+++ 17-21.js
@@ -0,0 +1,75 @@
+// Create Decimal Numbers with JavaScript
+// Create a variable myDecimal and give it a decimal value with a fractional part (e.g. 5.7).
+
+const ourDecimal = 5.7;
+
+// Solution
+
+const myDecimal = 5.7;
+
+//######################################################################
+
+// Multiply Two Decimals with JavaScript
+// Change the 0.0 so that product will equal 5.0.
+// const product = 2.0 * 0.0;
+
+// Solution
+
+const product = 2.0 * 2.5;
+
+//######################################################################
+
+// Divide One Decimal by Another with JavaScript
+// Change the 0.0 so that quotient will equal to 2.2.
+// const quotient = 0.0 / 2.0; // Change this line
+
+// Solution
+
+const quotient = 4.4 / 2.0;
+
+//######################################################################
+
+// Finding a Remainder in JavaScript
+// Set remainder equal to the remainder of 11 divided by 3 using the remainder (%) operator.
+// const remainder = 0;
+
+// Solution
+
+const remainder = 11 % 3;
+
+//######################################################################
+
+// Compound Assignment With Augmented Addition
+// Convert the assignments for a, b, and c to use the += operator.
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a = a + 12;
+b = 9 + b;
+c = c + 7;
+
+// Solution
+
+a += 12;
+b += 9;
+c += 7;
+
+//######################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
