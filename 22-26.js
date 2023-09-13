39a832d5b6ffd5b615cc096f237ed5d71c320af8
Andrei Slapik <Slapon4ik92@gmail.com>
2023-09-13 12:05:39 +0300

    add solutions 22-26

---
+++ 22-26.js
@@ -0,0 +1,81 @@
+// Compound Assignment With Augmented Subtraction
+// Convert the assignments for a, b, and c to use the -= operator.
+/* let a = 11;
+let b = 9;
+let c = 3;
+ */
+// Only change code below this line
+a = a - 6;
+b = b - 15;
+c = c - 1;
+
+// Solution
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+//#####################################################################
+
+// Compound Assignment With Augmented Multiplication
+// Convert the assignments for a, b, and c to use the *= operator.
+/* let a = 5;
+let b = 12;
+let c = 4.6; */
+
+// Only change code below this line
+a = a * 5;
+b = 3 * b;
+c = c * 10;
+
+// Solution
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+//######################################################################
+
+// Compound Assignment With Augmented Division
+// Convert the assignments for a, b, and c to use the /= operator.
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a = a / 12;
+b = b / 4;
+c = c / 11;
+
+// Solution
+
+a /= 12;
+b /= 4;
+c /= 11;
+
+//######################################################################
+
+// Escaping Literal Quotes in Strings
+/* Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would see:
+I am a "double quoted" string inside "double quotes".  */
+
+// Solution
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+//######################################################################
+
+// Quoting Strings with Single Quotes
+// Change the provided string to a string with single quotes at the beginning and end and no escape characters.
+// Right now, the <a> tag in the string uses double quotes everywhere. You will need to change the outer quotes to single quotes so you can remove the escape characters.
+
+// const myStr2 = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>";
+
+// Solution
+
+const myStr2 = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+
+
+
