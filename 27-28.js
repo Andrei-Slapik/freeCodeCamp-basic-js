7ae9999535a6d446aaac91fc7cc4cf603bf02c13
Andrei Slapik <Slapon4ik92@gmail.com>
2023-09-17 10:08:06 +0300

    add solution 27,28

---
+++ 27-28.js
@@ -0,0 +1,25 @@
+//Escape Sequences in Strings
+/* Assign the following three lines of text into the single variable myStr using escape sequences.
+
+FirstLine
+    \SecondLine
+ThirdLine
+
+You will need to use escape sequences to insert special characters correctly. You will also need to follow the spacing as it looks above, with no spaces between escape sequences or words.
+
+Note: The indentation for SecondLine is achieved with the tab escape character, not spaces. */
+
+// Solution
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+//###################################################################
+
+//Concatenating Strings with Plus Operator
+/* Build myStr from the strings This is the start. and This is the end. using the + operator. Be sure to include a space between the two strings. */
+
+// Solution
+
+const myStr1 = "This is the start. " +"This is the end.";
+
+//###################################################################
