342cdef7a9798be03ea6bacd4b0e362da97c9af6
Andrei Slapik <Slapon4ik92@gmail.com>
2023-09-17 10:49:28 +0300

    add solution 31-33

---
+++ 31-33.js
@@ -0,0 +1,33 @@
+// Appending Variables to Strings
+// Set someAdjective to a string of at least 3 characters and append it to myStr using the += operator.
+
+// Solution
+
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence."
+
+//###################################################################
+
+// Find the Length of a String
+// Use the .length property to set lastNameLength to the number of characters in lastName.
+
+ // Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Solution
+
+lastNameLength = lastName.length;
+
+//###################################################################
+
+// Use Bracket Notation to Find the First Character in a String
+// Use bracket notation to find the first character in the lastName variable and assign it to firstLetterOfLastName.
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName1 = "Lovelace";
+
+// Solution
+
+firstLetterOfLastName = lastName1[0];
